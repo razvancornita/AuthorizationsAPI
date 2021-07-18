fac8a1070099eda04c7deda01cd308ae82123923
razvancornita <rcornita@intuitext.ro>
2021-07-18 12:47:00 +0300

    - added ControllerConfig (for Spring Beans)

---
+++ api/src/main/java/com/authorization/controller/ControllerConfig.java
@@ -0,0 +1,8 @@
+package com.authorization.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+public class ControllerConfig {}
