bb52a0ec4bf0f732127258ebd850225b79978cd1
razvancornita <rcornita@intuitext.ro>
2021-07-18 12:46:01 +0300

    - added forgotten annotation (for Mongo)

--- api/src/main/java/com/authorization/AuthorizationApplication.java
+++ api/src/main/java/com/authorization/AuthorizationApplication.java
@@ -4,9 +4,11 @@ import com.authorization.mongo.MongoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @Import(MongoConfiguration.class)
+@EnableMongoRepositories
 public class AuthorizationApplication {
     public static void main(final String[] args) {
         SpringApplication.run(AuthorizationApplication.class, args);
