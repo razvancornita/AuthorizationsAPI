f4b69b9d9d68aac87d74f0e1079807d652af8135
razvancornita <rcornita@intuitext.ro>
2021-07-18 14:33:14 +0300

    - handled forgotten exception

--- api/src/main/java/com/authorization/security/JwtTokenFilter.java
+++ api/src/main/java/com/authorization/security/JwtTokenFilter.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -49,6 +50,8 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (AuthorizationHeaderMissingException e) {
+
+        } catch (NoSuchElementException e) {
 
         }
 
